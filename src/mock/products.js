20bc78f89614a2d51ad97fbfb4df4694abe7dfc7
Santiago Virar <santiago.virar20@gmail.com>
2025-04-24 10:39:06 -0300

    commit 2

--- src/mock/products.js
+++ src/mock/products.js
@@ -10,12 +10,30 @@ const products = [
 },
 {
     id: "2",
+    title: "Remera Adidas Essentials Trifolio",
+    price: 43500,
+    category: "remeras",
+    stock: 5,
+    description: "Una remera adidas suave que destaca el logo del Trifolio.",
+    image: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/7558df97cd6c48d09e67bff263fdd9b2_9366/Remera_Essentials_Trifolio_Verde_JN0235_01_laydown.jpg",
+},
+{
+    id: "3",
     title: "Zapatillas Adidas Dame Certified 3",
     price: 122500,
     category: "zapatillas",
     stock: 5,
     description: "Zapatillas de básquet exclusivas de Damian Lillard y adidas.",
     image: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/f0ccc58c7b1d48bb85a376ca011005d3_9366/Zapatillas_Dame_Certified_3_Verde_JI1542_04_standard.jpg",
+},
+{
+    id: "4",
+    title: "Zapatillas Adidas D.O.N. Issue 6",
+    price: 220500,
+    category: "zapatillas",
+    stock: 5,
+    description: "Zapatillas exclusivas de Donovan Mitchell y adidas Basketball",
+    image: "https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/5a1b9c2a5b5e42c29f1c53b1860f4324_9366/Zapatilla_D.O.N._Issue_6_Rojo_JI1547_01_00_standard.jpg",
 },
 ];
 
